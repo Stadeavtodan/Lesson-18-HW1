26452c618ba10c4cd18cdf82ceeefab25e4812e2
YehorPylypenko <stadeavtodan@gmail.com>
2025-07-04 16:04:01 +0200

    Added employee structure

---
+++ Employee.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Employee{
+    char secondName[100] = "undefined";
+    int bornYear = 0;
+    char position[50] = "undefined";
+    double salary = 0;
+    char[100] = "undefined";
+};
